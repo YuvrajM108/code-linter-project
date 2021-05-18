aec62b7e93294eef02048c901921c83b81d7ecf1
YuvrajM108 <yuvrajmanoo1@gmail.com>
2021-05-18 13:56:39 -0400

    Add initializer to code_section & use it in main to check for proper use of curly braces

--- js_code_linter.rb
+++ js_code_linter.rb
@@ -1,4 +1,4 @@
-require_relative '../lib/code_section'
+require_relative './lib/code_section'
 
 def semicolon_exception(snippet)
   return true if snippet.include? 'function'
@@ -12,18 +12,33 @@ end
 
 File.open('code.js', 'r') do |file|
   lines = file.readlines
+  tab_no = 0
+  sections = []
+  section_index = 0
   lines.each_with_index do |line, number|
     unless line.strip.empty?
       if semicolon_exception(line)
         puts "Forgot open curly braces ({) at line #{number + 1}." if line.gsub(/\s+/,
                                                                                 '')[line.gsub(/\s+/,
                                                                                               '').length - 1] != '{'
+        if line.gsub(/\s+/, '')[line.gsub(/\s+/, '').length - 1] == '{'
+          tab_no += 1
+          sections[section_index] = CodeSection.new(line, tab_no, number, true)
+          section_index += 1
+        end
       elsif line[line.length - 2] != ';' && line.gsub(/\s+/, '') != '}'
         if line.include? ';'
           puts "Line #{number + 1} must end with a semicolon (;) and no characters should come after the semicolon (;)."
         end
         puts "Missing semicolon (;) at line #{number + 1}." unless line.include? ';'
 
+      elsif line.gsub(/\s+/, '') == '}'
+        if sections[section_index - 1]&.is_open
+          sections[section_index - 1].is_open = false
+          tab_no -= 1
+        else
+          puts "Remove extra closing curely braces (}) at line #{number}"
+        end
       end
     end
   end
--- lib/code_section.rb
+++ lib/code_section.rb
@@ -1,3 +1,11 @@
 class CodeSection
-  
+  attr_reader :start_line, :indentaion_no, :line_no
+  attr_accessor :is_open
+
+  def initialize(start, tabs, line, opened)
+    @start_line = start
+    @indentaion_no = tabs * 2
+    @line_no = line
+    @is_open = opened
+  end
 end
