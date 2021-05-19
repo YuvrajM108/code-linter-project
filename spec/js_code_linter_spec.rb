49773ae0c65c9ccd9df53b3f8d3200b970f8647d
YuvrajM108 <yuvrajmanoo1@gmail.com>
2021-05-19 16:27:13 -0400

    Add test cases in which lines containing 'if' & 'else' are passed into semicolon_exception?

--- spec/js_code_linter_spec.rb
+++ spec/js_code_linter_spec.rb
@@ -4,4 +4,10 @@ describe '#semicolon_exception?' do
   it 'returns true if "function" is included in the line of code passed into the method' do
     expect(semicolon_exception?('function sum(x, y) {')).to eql(true)
   end
+  it 'returns true if "if" is included in the line of code passed into the method' do
+    expect(semicolon_exception?('if(x > y) {')).to eql(true)
+  end
+  it 'returns true if "else" is included in the line of code passed into the method' do
+    expect(semicolon_exception?('else {')).to eql(true)
+  end
 end
