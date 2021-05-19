87d271ee8d3c1262508a80a5bedbd4a9b410941c
YuvrajM108 <yuvrajmanoo1@gmail.com>
2021-05-19 16:07:40 -0400

    Add negative test case for correct_indentation? method & fix linter errors

--- spec/code_section_spec.rb
+++ spec/code_section_spec.rb
@@ -3,8 +3,11 @@ require_relative '../lib/code_section'
 describe CodeSection do
   describe '#correct_indentation?' do
     let(:section) { CodeSection.new('if (x > y) {', 2, 12, true) }
-    it 'returns true if line of code has the correct indentation of 4 spaces' do
+    it 'returns true if line of code passed into code_indentation? has correct indentation of 4 spaces' do
       expect(section.correct_indentation?('    return x + "is larger than" + y')).to eql(true)
+    end
+    it 'returs false if line of code passed into code_indentation? has incorrect indentation' do
+      expect(section.correct_indentation?('  return x + "is larger than" + y')).to eql(false)
     end
   end
 end
