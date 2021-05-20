ddb59b9cfc670d42a4ed9658648c81632e902a92
YuvrajM108 <yuvrajmanoo1@gmail.com>
2021-05-20 19:12:53 -0400

    Add RSpec test cases for in_comment? method

--- spec/js_line_spec.rb
+++ spec/js_line_spec.rb
@@ -1,5 +1,24 @@
 require_relative '../lib/js_line'
 
+describe '#in_comment?' do
+  it 'returns true if "/*" is included in the line of code and "*/" is not in the line of code' do
+    line = JSLine.new('/* This is a multi-line comment')
+    expect(line.in_comment?(false, 'let x = 4;')).to eql(true)
+  end
+  it 'returns true if currently in a comment & previous line does not include "*/"' do
+    line = JSLine.new('This is another line in a comment')
+    expect(line.in_comment?(true, '/* This is a multi-line comment')).to eql(true)
+  end
+  it 'returns false if previous line includes "*/", if currently in a comment, & if line does not include "/*"' do
+    line = JSLine.new('let y = 7;')
+    expect(line.in_comment?(true, '*/')).to eql(false)
+  end
+  it 'returns false if line does not pass any of the above cases' do
+    line = JSLine.new('let sum = x + y;')
+    expect(line.in_comment?(false, 'let y = 7;')).to eql(false)
+  end
+end
+
 describe '#semicolon_exception?' do
   it 'returns true if "function" is included in the line of code' do
     line = JSLine.new('function sum(x, y) {')
