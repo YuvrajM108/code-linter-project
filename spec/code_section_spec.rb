47beeb7b1ab52c98c0b474adecf7ad3a906c2409
YuvrajM108 <yuvrajmanoo1@gmail.com>
2021-05-20 23:14:06 -0400

    Fix number of arguments for CodeSection initialize in code_section_spec

--- spec/code_section_spec.rb
+++ spec/code_section_spec.rb
@@ -2,7 +2,7 @@ require_relative '../lib/code_section'
 
 describe CodeSection do
   describe '#correct_indentation?' do
-    let(:section) { CodeSection.new('if (x > y) {', 2, 12, true) }
+    let(:section) { CodeSection.new(2, 12, true) }
     it 'returns true if line of code passed into code_indentation? has correct indentation of 4 spaces' do
       expect(section.correct_indentation?('    return x + "is larger than" + y')).to eql(true)
     end
